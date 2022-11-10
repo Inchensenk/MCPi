aa2bf5115e3660dea244d715aeafee408116634b
Inchensenk <aab8117@gmail.com>
2022-11-10 17:34:39 +0300

    комменты

--- MCPi/Program.cs
+++ MCPi/Program.cs
@@ -1,37 +1,58 @@
-﻿namespace MyApp 
+﻿namespace MCPi
 {
     public class Program
     {
-        static double sum = 0;
+        //static double sum = 0;
 
-        static void Method_Monte_Karlo(object num_p)
+        /// <summary>
+        /// Расчет числа Пи методом Монте-Карло
+        /// </summary>
+        /// <param name="allPoints">Общее колличество точек</param>
+        static void MonteKarlo(object allPoints)
         {
-            int n_point = (int)num_p;
+            
+            int amountPointsInSquare = (int)allPoints;//Общее колличество точек
             double pi;
-            double n_circle = 0;
+            double amountPointsInCircle = 0;//колличество точек попавших в круг
             Random r = new Random();
-            for (int i = 0; i < n_point; i++)
+
+            for (int i = 0; i < amountPointsInSquare; i++)
             {
+                
                 if (IsPointInCircle(1.0, r.NextDouble(), r.NextDouble()))
                 {
-                    n_circle++;
+                    amountPointsInCircle++;
                 }
             }
-            pi = (4 * n_circle) / (double)n_point;
+
+            pi = (4 * amountPointsInCircle) / (double)amountPointsInSquare;
+
             Console.WriteLine(pi);
         }
 
+        /// <summary>
+        /// Вычесление принадлежности точки кругу
+        /// </summary>
+        /// <param name="R">Радиус круга</param>
+        /// <param name="x">Коордиината x</param>
+        /// <param name="y">Коордиината y</param>
+        /// <returns>Если точка принадлежит кругу, то функция возвращает 1</returns>
         static bool IsPointInCircle(double R, double x, double y)
         {
-            return ((x * x + y * y) <= R * R);
+            return Math.Pow((x - 1), 2) + Math.Pow((y - 1), 2) <= Math.Pow(R, 2);
         }
 
-        static void Threads(int numThreads, int num)
+        /// <summary>
+        /// Пулл потоков
+        /// </summary>
+        /// <param name="amountThreads">Колличество потоков в пулле</param>
+        /// <param name="num">Общее колличество точек</param>
+        static void Threads(int amountThreads, int num)
         {
-            ThreadPool.SetMaxThreads(numThreads, 0);
-            for (int i = 0; i < numThreads; i++)
+            ThreadPool.SetMaxThreads(amountThreads, 0);
+            for (int i = 0; i < amountThreads; i++)
             {
-                ThreadPool.QueueUserWorkItem(new WaitCallback(Method_Monte_Karlo), num);
+                ThreadPool.QueueUserWorkItem(new WaitCallback(MonteKarlo), num);
             }
         }
 
@@ -48,3 +69,15 @@
         }
     }
 }
+
+/*
+ QueueUserWorkItem(WaitCallback, Object): Помещает метод в очередь на выполнение и указывает объект, 
+содержащий данные для использования методом. Метод выполняется, когда становится доступен поток из пула потоков.
+
+ThreadPool: Предоставляет пул потоков, который можно использовать для выполнения задач, отправки рабочих элементов,
+обработки асинхронного ввода-вывода, ожидания от имени других потоков и обработки таймеров.
+
+ThreadPool.SetMaxThreads: Задает количество запросов к пулу потоков, которые могут быть активными одновременно. 
+Все запросы, превышающие это количество, остаются в очереди до тех пор, пока потоки пула не станут доступны.
+
+ */
